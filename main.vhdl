257bdf99c4400942683aeffc836d69327ae2c382
cmpunk <vaibhavraj.v11@gmail.com>
2021-05-20 16:57:34 +0530

    Main changed

---
+++ main.vhdl
@@ -0,0 +1,20 @@
+---
+--UART main file for entity and architecture
+---
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity main is
+  port (
+	--TODO
+  ) ;
+end entity ; -- main
+
+architecture struct of main is
+
+	signal 
+
+begin
+--TODO
+end architecture ; -- struct
